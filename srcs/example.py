d4f0a56840dc44836bac03b96e432ed006c92b85
Kwanho Kim <kkim@cx1r5s4.42seoul.kr>
2023-02-21 12:27:36 +0900

    CSP fix

--- srcs/example.py
+++ srcs/example.py
@@ -19,7 +19,7 @@ d1 = np.dot(rot1, [1,0])
 d2 = np.dot(rot2, [1,0])
 
 # Plot the generated data and their directions
-plt.subplot(1, 2, 1)
+plt.subplot(1, 3, 1)
 plt.scatter(data1[:,0], data1[:,1])
 plt.scatter(data2[:,0], data2[:,1])
 plt.plot([0, d1[0]]*int(np.max(data1)), [0, d1[1]]*int(np.max(data1)), linewidth=2)
@@ -53,7 +53,8 @@ data2_centered = data2 - mean2
 # Cov(X, Y) = E[(X - E(X) * (Y - E(Y))]
 cov1 = np.dot(data1_centered.T, data1_centered) / data1_centered.shape[0]
 cov2 = np.dot(data2_centered.T, data2_centered) / data2_centered.shape[0]
-
+# cov1 = np.cov(X1)
+# cov2 = np.cov(X2)
 
 # check is there NAN in the value
 # if	False in np.isnan(data1) or np.isnan(data2):
@@ -62,7 +63,7 @@ cov2 = np.dot(data2_centered.T, data2_centered) / data2_centered.shape[0]
 
 # eigen vector and eigen value
 
-[w, v] = np.linalg.eig(
+[w, v] = np.linalg.eigh(
 			np.dot(
 				np.linalg.inv(
 					np.sqrt(cov1)
@@ -75,14 +76,12 @@ cov2 = np.dot(data2_centered.T, data2_centered) / data2_centered.shape[0]
 				)
 			)
 		)
-print(w.shape)
-print("V : ", v, v.shape)
 
 X1_CSP = np.dot(v.T, X1)
 X2_CSP = np.dot(v.T, X2)
 
 # Plot the results
-plt.subplot(1,2,2)
+plt.subplot(1, 3, 2)
 plt.scatter(X1_CSP[0,:], X1_CSP[1,:])
 plt.scatter(X2_CSP[0,:], X2_CSP[1,:])
 plt.legend(['class 1', 'class 2'])
@@ -92,56 +91,30 @@ plt.title('After CSP filtering')
 plt.xlabel('Channel 1')
 plt.ylabel('Channel 2')
 
-# eigen vector and eigen value 2
-# print("debug1")
-# def power_iteration(A, num_simulations):
-#     b_k = np.random.rand(A.shape[1])
+# plot third one
 
-#     for _ in range(num_simulations):
-#         b_k1 = np.dot(A, b_k)
+from scipy import linalg
+[w, v] = linalg.eigh(
+	cov1,
+	cov1 + cov2
+)
+print(w.shape)
+print("V : ", v, v.shape)
 
-#         b_k1_norm = np.linalg.norm(b_k1)
-#         b_k = b_k1 / b_k1_norm
+X1_CSP = np.dot(v.T, X1)
+X2_CSP = np.dot(v.T, X2)
 
-#     return b_k
+# Plot the results
+plt.subplot(1, 3, 3)
+plt.scatter(X1_CSP[0,:], X1_CSP[1,:])
+plt.scatter(X2_CSP[0,:], X2_CSP[1,:])
+plt.legend(['class 1', 'class 2'])
+plt.axis('equal')
+plt.grid()
+plt.title('After CSP filtering')
+plt.xlabel('Channel 1')
+plt.ylabel('Channel 2')
 
-# def get_eigen(A):
-#     eigen_vectors = np.array([])
-#     for i in range(A.shape[0]):
-#         eigen_vector = power_iteration(A, i)
-#         eigen_vectors = np.append(eigen_vectors, eigen_vector)
-
-#     eigen_values = np.array([np.dot(A, eigen_vector) / eigen_vector for eigen_vector in eigen_vectors])
-#     return eigen_values, eigen_vectors
-# print("debug2")
-# eigen_values, eigen_vectors = get_eigen(
-# 	np.dot(
-# 		np.linalg.inv(
-# 			np.sqrt(cov1)
-# 		),
-# 		np.dot(
-# 			cov2,
-# 			np.linalg.inv(
-# 				np.sqrt(cov1)
-# 			)
-# 		)
-# 	)
-# )
-# print("debug3")
-# X1_CSP = np.dot(eigen_vectors.T, X1)
-# X2_CSP = np.dot(eigen_vectors.T, X2)
-
-# print("debug4")
-# # Plot the results
-# plt.subplot(1,3,3)
-# plt.scatter(X1_CSP[0,:], X1_CSP[1,:])
-# plt.scatter(X2_CSP[0,:], X2_CSP[1,:])
-# plt.legend(['class 1', 'class 2'])
-# plt.axis('equal')
-# plt.grid()
-# plt.title('After CSP filtering')
-# plt.xlabel('Channel 1')
-# plt.ylabel('Channel 2')
 
 plt.show()
 
